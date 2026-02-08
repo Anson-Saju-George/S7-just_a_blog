f7bbc1a13376a7bd7f5df1b923e5579bb96dc513
Anson Saju George <ansonsaju@karunya.edu.in>
2026-02-08 18:39:58 +0530

    Added base url.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
     return {
       
       plugins: [react(), tailwindcss()],
+      base: '/just-a-blog/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
